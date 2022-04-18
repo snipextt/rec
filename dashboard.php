7e30a57d8afba61111ed54c561d4a6f3f23f8238
Saurav Singh <53917524+snipextt@users.noreply.github.com>
2022-04-18 09:00:57 +0530

    dashboard

--- dashboard.php
+++ dashboard.php
@@ -33,9 +33,9 @@ $result = $conn->query($userListingSql);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://unpkg.com/flowbite@1.4.1/dist/flowbite.js"></script>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://unpkg.com/flowbite@1.4.1/dist/flowbite.min.css" />
-    <script src="https://unpkg.com/flowbite@1.4.1/dist/flowbite.js"></script>
     <title>Dashboard</title>
 </head>
 
@@ -109,91 +109,34 @@ $result = $conn->query($userListingSql);
 
                 <?php
                 foreach ($result->fetch_all() as $listing) {
+                    $listingId = $listing[0];
                     $image = $listing[4];
                     $title = $listing[2];
                     $description = $listing[3];
                     $price = $listing[5];
-                    echo "<div class='md:col-span-2 lg:col-span-1'><div class='h-full py-8 px-6 space-y-6 rounded-xl border border-gray-200 bg-white'><img src='$image' class='h-72 m-auto' />
+                    echo "<div data-id='$listingId' class='listing md:col-span-2 lg:col-span-1'><div class='h-full py-8 px-6 space-y-6 rounded-xl border border-gray-200 bg-white'><img src='$image' class='h-72 m-auto' />
                     <h4 class='text-center text-lg font-semibold'>$title</h4>
                     <p>$description</p>
                     <p>Price/hour: ₹ $price</p>
-                    <button class='w-full bg-blue-600 text-white font-semibold py-2 px-4 rounded-md' data-modal-toggle='edit-listing-modal'>Edit</button>
-                    <button class='w-full bg-red-600 text-white font-semibold py-2 px-4 rounded-md' data-modal-toggle='popup-modal'>Delete</button>
                     </div></div>";
                 }
                 ?>
             </div>
-            <div id="popup-modal" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 md:inset-0 h-modal md:h-full">
-                <div class="relative p-4 w-full max-w-md h-full md:h-auto">
-                    <!-- Modal content -->
-                    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                        <!-- Modal header -->
-                        <div class="flex justify-end p-2">
-                            <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="popup-modal">
-                                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-                                </svg>
-                            </button>
-                        </div>
-                        <!-- Modal body -->
-                        <div class="p-6 pt-0 text-center">
-                            <svg class="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                            </svg>
-                            <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this listing?</h3>
-                            <button data-modal-toggle="popup-modal" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
-                                Yes
-                            </button>
-                            <button data-modal-toggle="popup-modal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No, cancel</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div id="add-listing-modal" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
-                <div class="relative p-4 w-full max-w-3xl h-full md:h-auto">
-                    <div class="relative bg-white rounded-lg shadow">
-                        <div class="flex justify-between items-center p-5 rounded-t border-b ">
-                            <h3 class="text-xl font-medium text-gray-900 ">
-                                Add Listing
-                            </h3>
-                            <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center" data-modal-toggle="add-listing-modal">
-                                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-                                </svg>
-                            </button>
-                        </div>
-                        <!-- Modal body -->
-                        <div class="p-6 space-y-6">
-                            <form action="./addListing.php" method="POST" enctype="multipart/form-data">
-                                <div class="mb-6">
-                                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Listing Name</label>
-                                    <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Name" name="name" required>
-                                </div>
-                                <div class="mb-6">
-
-                                    <label for="description" class="block mb-2 text-sm font-medium">Description</label>
-                                    <textarea id="description" name="description" rows="4" class="block p-2.5 w-full text-sm rounded-lg border focus:ring-blue-500 focus:border-blue-50 border-gray-300" placeholder="Sescription"></textarea>
-                                </div>
-                                <div class="mb-6">
-                                    <label for="pricing" class="block mb-2 text-sm font-medium text-gray-900">Pricing Per Hour</label>
-                                    <input id="pricing" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="₹0" name="price" required>
-                                </div>
-                                <div class="mb-6">
-                                    <label for="pricing" class="block mb-2 text-sm font-medium text-gray-900">Upload Image</label>
-                                    <input type="file" name="fileToUpload" required>
-                                </div>
-                                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
             <?php
 
-            require './partials/addListingForm.php'
+            require './partials/addListingForm.php';
+
             ?>
         </div>
     </div>
+    <script>
+        const listings = document.querySelectorAll('.listing');
+        Array.from(listings).forEach(listing => {
+            listing.addEventListener('click', (e) => {
+                window.location.href = './bookingInfo.php?listingId=' + listing.dataset.id;
+            })
+        })
+    </script>
 </body>
 
 </html>
